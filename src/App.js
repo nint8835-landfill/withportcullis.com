ad511480c48642ed5077646030f48bdbbaefa789
Riley Flynn <riley@rileyflynn.me>
2021-10-10 16:14:18 -0230

    Add docs link

--- src/App.js
+++ src/App.js
@@ -6,28 +6,33 @@ import {
   Heading,
   Stack,
   VStack,
+  Text,
 } from '@chakra-ui/react';
-import { GoMarkGithub } from 'react-icons/go';
+import { GoMarkGithub, GoBook } from 'react-icons/go';
 
-const repos = ['nint8835/withportcullis.com', 'nint8835/portcullis'];
+const repos = [
+  'nint8835/withportcullis.com',
+  'nint8835/portcullis',
+  'nint8835/docs.withportcullis.com',
+];
 
 function App() {
   return (
     <Center height="100%">
       <VStack textAlign="center">
         <Heading>Portcullis</Heading>
-        <Box>
+        <Text>
           This is a placeholder page, so this URL isn't a <Code>404</Code>.
           Actual site{' '}
           <Box as="span" color="purple.400">
             coming soon
           </Box>
           .
-        </Box>
-        <Box>
+        </Text>
+        <Text>
           In the mean time, why don't you check out some of the associated
           repos:
-        </Box>
+        </Text>
         <Stack direction={['column', 'row']}>
           {repos.map((repo) => (
             <Button
@@ -40,6 +45,20 @@ function App() {
             </Button>
           ))}
         </Stack>
+        <Box>
+          <Text>Or, check out the docs:</Text>
+        </Box>
+        <Box>
+          <Button
+            leftIcon={<GoBook />}
+            as="a"
+            href="https://docs.withportcullis.com"
+            colorScheme="purple"
+            variant="outline"
+          >
+            Docs
+          </Button>
+        </Box>
       </VStack>
     </Center>
   );
