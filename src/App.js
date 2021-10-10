35fba9f09ce2b30c2e9b8ada991b123a01c52934
Riley Flynn <riley@rileyflynn.me>
2021-10-10 03:07:47 -0230

    Fix up fomatting slightly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import {
-  Center,
-  Container,
-  Heading,
   Box,
-  Code,
   Button,
-  HStack,
+  Center,
+  Code,
+  Heading,
+  Stack,
+  VStack,
 } from '@chakra-ui/react';
 import { GoMarkGithub } from 'react-icons/go';
 
@@ -13,11 +13,8 @@ const repos = ['nint8835/withportcullis.com', 'nint8835/portcullis'];
 
 function App() {
   return (
-    <Center
-      height="100%"
-      // background="linear-gradient(180deg, rgba(20,20,20,1) 0%, rgba(0,0,0,1) 100%);"
-    >
-      <Container textAlign="center">
+    <Center height="100%">
+      <VStack textAlign="center">
         <Heading>Portcullis</Heading>
         <Box>
           This is a placeholder page, so this URL isn't a <Code>404</Code>.
@@ -31,21 +28,19 @@ function App() {
           In the mean time, why don't you check out some of the associated
           repos:
         </Box>
-        <Center marginTop="4">
-          <HStack>
-            {repos.map((repo) => (
-              <Button
-                leftIcon={<GoMarkGithub />}
-                as="a"
-                href={`https://github.com/${repo}`}
-                colorScheme="purple"
-              >
-                {repo}
-              </Button>
-            ))}
-          </HStack>
-        </Center>
-      </Container>
+        <Stack direction={['column', 'row']}>
+          {repos.map((repo) => (
+            <Button
+              leftIcon={<GoMarkGithub />}
+              as="a"
+              href={`https://github.com/${repo}`}
+              colorScheme="purple"
+            >
+              {repo}
+            </Button>
+          ))}
+        </Stack>
+      </VStack>
     </Center>
   );
 }
